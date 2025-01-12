6a2ad895bdc0c31017ed392276caf24624763662
umesh <umesharma.us11@gmail.com>
2025-01-12 21:21:52 +0530

    Implemented the header for the home screen screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,25 +1,32 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import React from 'react';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import images from '@/constants/images';
+import icons from '@/constants/icons';
 
 const Index = () => {
   const id = 1;
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text className="font-bold text-2xl my-10">
-        Welcome to React Native Project
-      </Text>
-      <Link href="/sign_in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="../properties/1">Property</Link>
-    </View>
+    <SafeAreaView className="bg-white h-full pt-2">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between">
+          <View className="flex flex-row gap-4">
+            <Image source={images.avatar} className="size-14" />
+            <View className="flex flex-col items-start justify-center">
+              <Text className="mb-1 text-black-100 text-sm">Good Morning</Text>
+              <Text className=" text-black text-lg font-rubik-bold">
+                Umesh Sharma
+              </Text>
+            </View>
+          </View>
+          <View className="relative">
+            <Image source={icons.bell} className="size-6 " />
+            <Image source={icons.bell} className="size-6 " />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 export default Index;
