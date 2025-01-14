251f6d43e8d1d5da846bc08ce7cdee075e0ff9a4
umesh <umesharma.us11@gmail.com>
2025-01-14 21:47:45 +0530

    Implemented the stylig, added function to set the selected item

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,23 +1,37 @@
 import { View, Text, ScrollView, Pressable } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { categories } from '@/constants/data';
 import { useLocalSearchParams } from 'expo-router';
 
 const Filters = () => {
   const params = useLocalSearchParams<{ filter?: string }>();
-
+  const [selectedCategory, setSelectedCategory] = useState(
+    params.filter || 'All'
+  );
   const handleCategoryPress = (category: string) => {
-    console.log(category);
+    setSelectedCategory(category);
   };
-
+  console.log(selectedCategory);
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
       {categories.map((item, index) => (
         <Pressable
           onPress={() => handleCategoryPress(item.category)}
-          className="bg-primary-300 mx-2 px-5 py-2 rounded-full "
+          className={` mx-2 px-5 py-2 rounded-full ${
+            selectedCategory === item.category
+              ? 'bg-primary-300 text-white'
+              : 'bg-primary-100'
+          }`}
         >
-          <Text className="text-white font-rubik-bold">{item.title}</Text>
+          <Text
+            className={`  ${
+              selectedCategory === item.category
+                ? ' text-white font-rubik-bold'
+                : 'text-black'
+            }`}
+          >
+            {item.title}
+          </Text>
         </Pressable>
       ))}
     </ScrollView>
